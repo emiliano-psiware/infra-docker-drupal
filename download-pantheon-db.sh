34342c049b008f894c668e7965ea7e5764b99de9
Emiliano Roldan <emiliano@psiware.com.ar>
2023-08-24 11:23:19 -0300

    agrego commentario a download-pantheon-db.sh

--- download-pantheon-db.sh
+++ download-pantheon-db.sh
@@ -1,2 +1,3 @@
+#La url de pantheon va entre "" sh download-pantheon-db.sh "https://storage.googleapis.com/gcs-pantheon-backups/ ...."
 wget -O dump.sql.gz "$1" --no-check-certificate
 gunzip dump.sql.gz
